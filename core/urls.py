2aab10d954313448156f309ecfb672d81836fefe
marketing <marketing@smartbuildingapps.com>
2020-12-24 11:49:06 -0500

    working on chartjs2

--- core/urls.py
+++ core/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('plot/', plot.as_view(), name='plot'),
+    re_path('plot/', plot.as_view(), name='plot'),
     path('admin/', admin_site.urls),
-    path('chart/', line_chart, name='line_chart'),
-    path('chartJSON/', line_chart_json, name='line_chart_json'),
-    # re_path(r'chartJSON/\?(?:model=(?P<model>\w+))\&(?:ids=(?P<ids>[0-9,]+))', line_chart_json, name='line_chart_json'),
+    # path('chart/', line_chart, name='line_chart'),
+    path('chartJSON/' , LineChartJSONView.as_view(), name='line_chart_json'),
+    # re_path(r'chartJSON/(?:model=(?P<model>\w+))?\&(?:ids=(?P<ids>[0-9,]+))?', line_chart_json, name='line_chart_json'),
 ]
--- core/views.py
+++ core/views.py
@@ -24,93 +24,45 @@ class plot(TemplateView):
     template_name = "admin/index1.html"
 
     def get_context_data(self, **kwargs):
-        print(kwargs)
-        print(type(kwargs))
         
         model=self.request.GET.get('model','')
-        ids=list(map(int,self.request.GET.get('ids','').split(',')))
-        print(model,ids)
-        data = super().get_context_data(**kwargs)
-        data['Spectra'] = Spectrum.objects.filter(eval('|'.join('Q(id='+str(pk)+')' for pk in ids)))
-        data['s1']=[i.y().tolist() for i in data['Spectra']]
+        ids=self.request.GET.get('ids','')
+        # ids=list(map(int,self.request.GET.get('ids','').split(',')))
+        data = super().get_context_data()
+
+        data["model"]=model
+        data["ids"]=ids
+
         return data
     
-    def get_labels(self):
-        """Return 7 labels for the x-axis."""
-        return ["January", "February", "March", "April", "May", "June", "July"]
-
-    def get_providers(self):
-        """Return names of datasets."""
-        return ["Central", "Eastside", "Westside"]
-
-    def get_data(self):
-        print(self.request.get_full_path())
-        """Return 3 datasets to plot."""
-        s1=Spectrum.objects.all()[0].y().tolist()
-        s2=Spectrum.objects.all()[1].y().tolist()
-        s3=s1[:25]+s2[25:]
-        return [s1,s2,s3]
-    # plt.plot(range(100))
-    # fig=plt.gcf()
-    # buf=io.BytesIO()
-    # fig.savefig(buf,format="png")
-    # buf.seek(0)
-    # string = base64.b64decode(buf.read())
-    # uri = urllib.parse.quote(string)
-    # return render(request,'admin/index1.html',{'data':uri})
-    # fig=ma(figure='my_figure')
-    # # return HttpResponse(str(dir(request))+str(request))
-    # response = HttpResponse(content_type="application/json")
-    # # print(dir(response),'\n',response.content)
-    # serializers.serialize("json", fig, stream=response)
-    # print(dir(response),'\n',response.content)
-    # return response
+   
 
 class LineChartJSONView(BaseLineChartView):
-    
-    def get_context_data(self, **kwargs):
-        print(kwargs)
-        print(type(kwargs))
-        
+
+    def spect2context(self, **kwargs):
+        self.dic ={"view": "<core.views.LineChartJSONView object at 0x000002F8626AE940>", "labels": ["January", "February", "March", "April", "May", "June", "July"], "datasets": [{"data": [75, 44, 92, 11, 44, 95, 35], "backgroundColor": "rgba(202, 201, 197, 0.5)", "borderColor": "rgba(202, 201, 197, 1)", "pointBackgroundColor": "rgba(202, 201, 197, 1)", "pointBorderColor": "#fff", "label": "Central", "name": "Central"}, {"data": [41, 92, 18, 3, 73, 87, 92], "backgroundColor": "rgba(171, 9, 0, 0.5)", "borderColor": "rgba(171, 9, 0, 1)", "pointBackgroundColor": "rgba(171, 9, 0, 1)", "pointBorderColor": "#fff", "label": "Eastside", "name": "Eastside"}, {"data": [87, 21, 94, 3, 90, 13, 65], "backgroundColor": "rgba(166, 78, 46, 0.5)", "borderColor": "rgba(166, 78, 46, 1)", "pointBackgroundColor": "rgba(166, 78, 46, 1)", "pointBorderColor": "#fff", "label": "Westside", "name": "Westside"}]}
+        dic = self.dic
         model=self.request.GET.get('model','')
         ids=list(map(int,self.request.GET.get('ids','').split(',')))
-        print(model,ids)
-        data = super().get_context_data(**kwargs)
-        data['Spectra'] = Spectrum.objects.filter(eval('|'.join('Q(id='+str(pk)+')' for pk in ids)))
-        data['s1']=[i.y().tolist() for i in data['Spectra']]
-        
+        print('spct2con:',model,ids)
+        context=super(BaseLineChartView, self).get_context_data(**kwargs)
+        context.update({'Spectra': Spectrum.objects.filter(eval('|'.join('Q(id='+str(pk)+')' for pk in ids)))})
+        context.update({'dic': dic})
+        return context
 
-        def self.get_labels(self,data):
-            """Return 7 labels for the x-axis."""
-            return ["January", "February", "March", "April", "May", "June", "July"]
+    def get_labels(self):
+        self.cont=self.spect2context()
+        x=list(map(int,self.cont['Spectra'].first().x()))
+        return x+[]
 
-        def self.get_providers(self):
-            """Return names of datasets."""
-            return ["Central", "Eastside", "Westside"]
+    def get_providers(self):
+        return [i.origin for i in self.cont['Spectra']]
 
-        def self.get_data(self):
-            print(self.request.get_full_path())
-            """Return 3 datasets to plot."""
-            s1=Spectrum.objects.all()[0].y().tolist()
-            s2=Spectrum.objects.all()[1].y().tolist()
-            s3=s1[:25]+s2[25:]
-            return [s1,s2,s3]
-            # return [[75, 44, 92, 11, 44, 95, 35],
-            #         [41, 92, 18, 3, 73, 87, 92],
-            #         [87, 21, 94, 3, 90, 13, 65]]
-
-    def ips2data(self,**kwargs):
-        
-        model=self.request.GET.get('model','')
-        ids=list(map(int,self.request.GET.get('ids','').split(',')))
-        print(model,ids)
-        data = super().get_context_data(**kwargs)
-        data['Spectra'] = Spectrum.objects.filter(eval('|'.join('Q(id='+str(pk)+')' for pk in ids)))
-        data['y_axis']=[i.y().tolist() for i in data['Spectra']]
-        data['origin']=[i.origin for i in data['Spectra']]
-        return data
+    def get_data(self):
+        return [i.y().tolist() for i in self.cont['Spectra']]
 
 
 
-line_chart = TemplateView.as_view(template_name='admin/index1.html')
-line_chart_json = LineChartJSONView.as_view()
+
+# line_chart = TemplateView.as_view(template_name='admin/index1.html')
+# line_chart_json = LineChartJSONView.as_view()
